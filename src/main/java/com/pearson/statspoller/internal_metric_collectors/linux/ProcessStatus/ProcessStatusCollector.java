318faa247c4961b3da9ce3ef608a3647fd64b604
Jeffrey Schmidt <jeffrey.schmidt@pearson.com>
2017-09-19 10:18:44 -0500

    import fix

--- src/main/java/com/pearson/statspoller/internal_metric_collectors/linux/ProcessStatus/ProcessStatusCollector.java
+++ src/main/java/com/pearson/statspoller/internal_metric_collectors/linux/ProcessStatus/ProcessStatusCollector.java
@@ -14,7 +14,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
-import org.codehaus.plexus.util.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
